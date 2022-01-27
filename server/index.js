5b49a4070e14d3db222c4fa8f59b8c4a96c24109
Fernanda Rodrigues da Silva <fernanda.rodrigues.cdc@gmail.com>
2022-01-26 21:34:34 -0800

    Connected to db

--- server/index.js
+++ server/index.js
@@ -1,8 +1,10 @@
 const express = require('express');
 const app = express();
+const mongoose = require('mongoose');
 const path = require('path');
 const cors = require('cors');
-
+require('dotenv').config();
+const PORT = process.env.PORT || 3000;
 app.use(express.json());
 app.use(express.urlencoded({extended: true}));
 app.use(cors());
@@ -12,8 +14,8 @@ app.get('/', function(req, res) {
   res.sendFile(path.join(__dirname, '../client/dist/index.html'));
 });
 
-
-app.listen(process.env.PORT || 3000, () => {
-  console.log('process.env.PORT', process.env.PORT);
-  console.log('App server running');
-})
+mongoose.connect(process.env.MONGO_ATLAS_DB, { useNewUrlParser: true, useUnifiedTopology: true })
+  .then(() => app.listen(PORT, () => {
+    console.log(`Connected to DB & Server Running on Port: ${PORT}`)
+  }))
+  .catch((error) => console.log(`${error} did not connect`));
